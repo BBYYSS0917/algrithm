89477fa676f63291a0c85d890a017cbaa8896ea1
Donquixote <826714738@qq.com>
2022-08-29 18:18:14 +0800

    binary search

---
+++ src/binary/BSNearRight.java
@@ -0,0 +1,54 @@
+package binary;
+
+/**
+ * @author BaiJY
+ * @date 2022/07/18
+ **/
+public class BSNearRight {
+    public static int nearestIndex(int[] arr, int value) {
+        int L = 0;
+        int R = arr.length - 1;
+        int index = -1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (arr[mid] >= value) {
+                index = mid;
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    // for test
+    public static int test(int[] arr, int value) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] <= value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
