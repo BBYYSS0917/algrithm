8335c596cff7865d233a1ba7d24fe9de924ae40a
Donquixote <826714738@qq.com>
2023-02-13 15:43:00 +0800

    leetcode fib

---
+++ src/dp/Fib.java
@@ -0,0 +1,29 @@
+package dp;
+
+/**
+ * @author BaiJY
+ * @date 2023/02/13
+ **/
+public class Fib {
+    public static void main(String[] args) {
+
+        System.out.println(fib(84));
+    }
+
+    public static int fib(int n) {
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int a = 1, b = 0;
+        for (int i = 2; i <= n; i++) {
+            a = a + b;
+            b = a - b;
+            //为了防止中途溢出，每次计算都需要取模
+            a %= 1000000007;
+        }
+
+        return a;
+    }
+}
