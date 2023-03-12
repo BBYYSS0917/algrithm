5fd349b466c04b5e34cdf3234c31d4c6a59e18d0
Donquixote <826714738@qq.com>
2023-03-12 23:08:06 +0800

    二分法查询

--- src/array/SearchNumTimes.java
+++ src/array/SearchNumTimes.java
@@ -6,36 +6,44 @@ package array;
  **/
 public class SearchNumTimes {
     public static int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        return 0;
+        int left=binarySearch(nums, target,true);
+        System.out.println("left===="+left);
+        int right=binarySearch(nums, target, false);
+        System.out.println("right===="+right);
+        if(nums[left]>target||nums[right]<target){
+            return 0;
+        }
+        if(nums[left]==nums[right]){
+            return right-left+1;
+        }
+        return right-left;
     }
 
-    public static int findLeft(int[] nums,int target) {
+
+    public static int binarySearch(int[] nums,int target,boolean isLeft){
         int left = 0;
         int right = nums.length - 1;
         while (right > left) {
-            int mid = (right - left) / 2;
-            if (nums[mid] > target) {
-                right = mid-1;
-            }
+
+            int mid = (right + left) / 2;
             if (nums[mid] < target) {
-                left = mid+1;
+                left=mid+1;
+            }else if(nums[mid]>target){
+                right=mid;
+            }else{
+                if(isLeft){
+                    right-=1;
+                }else{
+                    left+=1;
+                }
             }
-
         }
-        return 0;
-    }
-
-    public static int findRight(int[] nums) {
-
-        return 0;
+        return isLeft?left:right;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{5, 7, 7, 8, 8, 10};
-        int target = 8;
-        System.out.println(search(nums, target));
+        int[] nums = new int[]{1,2,3};
+        int target = 1;
+        System.out.println(binarySearch(nums, target,false));
     }
 }
