a3d2fbf2dbb609eff1a14a363365984f480cc327
Donquixote <826714738@qq.com>
2023-03-30 17:30:35 +0800

    剑指Offer50

--- src/string/FirstUniqueChar.java
+++ src/string/FirstUniqueChar.java
@@ -1,31 +1,52 @@
 package string;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
-public class FirstUniqueChar{
-
+public class FirstUniqueChar {
 
     public static char Solution(String s) {
-        TreeSet<Character> hashSet=new TreeSet();
-        char[] chars=s.toCharArray();
-        for(char c:chars){
-            if(hashSet.contains(c)){
-                hashSet.remove(c);
-            }else{
-                hashSet.add(c);    
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (!map.containsKey(c)) {
+                map.put(c, 0);
+            }
+            map.put(c, map.get(c) + 1);
+        }
+
+        for (char c : chars) {
+            if (map.get(c)==1) {
+                return c;
             }
         }
-        if(hashSet.size()>0){
-            return hashSet.iterator().next();
+
+        return ' ';
+
+    }
+
+
+    public static char Solution1(String s){
+
+        LinkedHashMap<Character,Integer> map=new LinkedHashMap<>();
+        for(char c :s.toCharArray()){
+            if(!map.containsKey(c)){
+                map.put(c,0);
+            }
+            map.put(c, map.get(c)+1);
+        }
+
+        for(char c:map.keySet()){
+            if(map.get(c)==1){
+                return c;
+            }
         }
 
         return ' ';
     }
 
     public static void main(String[] args) {
-        System.out.println(Solution("leetcode"));
-        System.out.println(Solution(""));
+        System.out.println(Solution1("leetcode"));
+        System.out.println(Solution1(""));
     }
 }
