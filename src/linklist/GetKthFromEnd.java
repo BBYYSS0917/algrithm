9fbf451e051cab52bc430dfdf5869c72b8eb6965
Donquixote <826714738@qq.com>
2023-04-27 14:38:05 +0800

    寻找单链表倒数第K个节点

---
+++ src/linklist/GetKthFromEnd.java
@@ -0,0 +1,34 @@
+package linklist;
+
+public class GetKthFromEnd {
+
+
+  private class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+  public ListNode solution(ListNode head, int k) {
+
+    ListNode p1=head;
+    ListNode p2=head;
+
+    for(int i=1;i<=(k-1);i++){
+        p1=p1.next;
+    }
+    while(p1.next!=null){
+        p1=p1.next;
+        p2=p2.next;
+    }
+    //倒数第K个节点
+
+    return p2;
+  }
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
